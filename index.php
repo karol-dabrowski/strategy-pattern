3b836cbf1a3dcccc59b8c57662bdba1ccb4e5b14
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-01-31 15:13:53 +0100

    Create a usage example in the index file

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+use App\CalorieCalculator;
+use App\Bmr\HarrisBenedictBmrCalculator;
+use App\ActivityLevel;
+use App\Gender;
+
+$harrisBenedictCalculator = new HarrisBenedictBmrCalculator();
+$calculator1 = new CalorieCalculator($harrisBenedictCalculator);
+
+echo 'Harris-Benedict: ';
+echo $calculator1->calculate(86, 191, 27, Gender::MALE, ActivityLevel::HIGH_ACTIVITY);
